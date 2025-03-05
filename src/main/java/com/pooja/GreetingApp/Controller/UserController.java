c246aaa96ae618e4c979ee8119ab55eb354dc9bd
pooja <pooja.gla_cs21@gla.ac.in>
2025-03-05 14:34:44 +0530

    user Login

--- src/main/java/com/pooja/GreetingApp/Controller/UserController.java
+++ src/main/java/com/pooja/GreetingApp/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.pooja.GreetingApp.Controller;
 
 import com.pooja.GreetingApp.Services.EmailService;
 import com.pooja.GreetingApp.Services.UserService;
+import com.pooja.GreetingApp.dto.LoginDTO;
 import com.pooja.GreetingApp.dto.RegisterDTO;
 import com.pooja.GreetingApp.model.User;
 import com.pooja.GreetingApp.utility.JwtUtility;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
@@ -41,6 +44,27 @@ public class UserController {
         String body = "Hello " + savedUser.getFullName() + ",\n\nYour account has been successfully created!";
         emailService.sendEmail(savedUser.getEmail(), subject, body);
         return "User Registered Successfully and Your Token is: " + token;
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@Valid@RequestBody LoginDTO loginUser){
+        Optional<User> userExists = userService.getUserByEmail(loginUser.getEmail());
+        if(userExists.isPresent()){
+            User user = userExists.get();
+            if(userService.matchPassword(loginUser.getPassword(),user.getPassword())){
+                String token = jwtUtility.generateToken(user.getEmail());
+                String subject = "Welcome Back to Our Platform!";
+                String body = "Hello " + user.getFullName() + ",\n\nYour account has been successfully Logged In! and Your Token is: "+token;
+                emailService.sendEmail(user.getEmail(), subject, body);
+                return "User Login Successfully and Token is: "+token;
+            }
+            else{
+                return "InValid Crendentials";
+            }
+        }
+        else{
+            return "User Not Found";
+        }
     }
 
 }
---
+++ src/main/java/com/pooja/GreetingApp/dto/LoginDTO.java
@@ -0,0 +1,37 @@
+package com.pooja.GreetingApp.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+}
